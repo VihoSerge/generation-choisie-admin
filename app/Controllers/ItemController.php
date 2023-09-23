d3dd76e7081e05d5d4f28ddde3f18e0baac49fe8
ODADJE <odadjerk@gmail.com>
2023-09-23 16:25:26 +0000

    adding mock data to podcastController

--- app/Controllers/ItemController.php
+++ app/Controllers/ItemController.php
@@ -12,22 +12,22 @@ class ItemController extends BaseController
     protected $item = [
         [
             'id' => 1,
-            'title' => 'Item1',
+            'name' => 'Item1',
             'url' => 'https://www.youtube.com/watch?v=QCgCsBmNG6k&pp=ygUQcHJpZXJlIGluc3BpcsOpZQ%3D%3D'
         ],
         [
             'id' => 2,
-            'title' => 'Item2',
+            'name' => 'Item2',
             'url' => 'https://www.youtube.com/watch?v=UlW_-ZNpUVQ&pp=ygUQcHJpZXJlIGluc3BpcsOpZQ%3D%3D'
         ],
         [
             'id' => 3,
-            'title' => 'Item3',
+            'name' => 'Item3',
             'url' => 'https://www.youtube.com/watch?v=ge-Li4YpEKM&pp=ygUQcHJpZXJlIGluc3BpcsOpZQ%3D%3D'
         ],
         [
             'id' => 4,
-            'title' => 'Item4',
+            'name' => 'Item4',
             'url' => 'https://www.youtube.com/watch?v=U66sqSgn4XE&pp=ygUQcHJpZXJlIGluc3BpcsOpZQ%3D%3D'
         ],
     ];
